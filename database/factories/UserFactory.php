c8519b5e624ddcaf458d1641ec7fd37b82010d5b
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-09-05 16:21:55 +0200

    update data user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,8 @@ class UserFactory extends Factory
             'email' => fake()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'name' => fake()->name(),
-            'surname'=> fake()->,
-            'dateOfBirth'=> fake()->,
+            'surname'=> fake()->word(),
+            'dateOfBirth'=> fake()->dateTime(),
         ];
     }
 
