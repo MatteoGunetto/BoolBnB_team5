2880b3cdf0c2d3c90223e715df3d2a4d9e660167
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-09-06 01:19:06 +0200

    correzione errore -seed SQLSTATE[22001]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('email', 128)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 32);
+            $table->string('password', 255);
             $table->string('name');
             $table->string('surname');
             $table->date('dateOfBirth');
