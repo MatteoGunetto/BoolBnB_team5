2619706f79d16623d674ec39f0082e98d8b2c664
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-09-06 12:39:42 +0200

    view e controller "Apartments" creati

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ApartmentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Apartment/show', [ApartmentController :: class, "show"]) -> name('Apartment.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
