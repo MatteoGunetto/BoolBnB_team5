b4657d2f8e97dc5826f39ba58ecd572fd3a638af
FRAALES <alessandrofranco1992@gmail.com>
2023-09-07 16:22:31 +0200

    modificate dashboard e index

--- resources/views/Apartment/index.blade.php
+++ resources/views/Apartment/index.blade.php
@@ -1,42 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
 
-    <!-- Barra di navigazione -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">BoolBNB</a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('Apartment.index') }}">Home</a>
-                </li>
-                {{-- <li class="nav-item">
-                    <a class="nav-link" href="{{ route('Apartment/show/{id}') }}">I tuoi appartamenti</a>
-                </li> --}}
-                <li>
-                    <a class="nav-link" href="{{ route('Apartment.create') }}">Aggiungi un appartamento</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="{{ route('Apartment.edit') }}">Modifica un appartamento</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="pagamento">Metti in evidenza</a>
-                </li>
-            </ul>
-            <!-- Ricerca avanzata -->
-            <form class="form-inline ml-auto">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Cerca..." aria-label="Cerca...">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="button">Cerca</button>
-                    </div>
-                </div>
-            </form>
+
+<div class="container">
+    <!-- Ricerca avanzata -->
+    <form class="form-inline ml-auto my-5">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Cerca un appartamento per le tue vacanze" aria-label="Cerca...">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Cerca</button>
+            </div>
         </div>
-    </nav>
+    </form>
 
     <!-- Elenco case vacanze in evidenza -->
     <h1 class="mt-4">Case Vacanze in Evidenza</h1>
+
     <div class="card-deck mt-3">
         @foreach ($apartments as $apartment)
         <div class="card">
@@ -47,30 +26,6 @@
                 <p class="card-text">Location: {{ $apartment->address }}</p>
                 <a href="{{ route('Apartment.show', $apartment->id) }}" class="btn btn-primary">Dettagli</a>
             </div>
-        </div>
-        @endforeach
-    </div>
-
-    <!-- Questa parte l'ha fatta Matte, chiedi prima di toccarla -->
-    <h1>APARTMENT INDEX</h1>
-    <div class="row row-cols-3">
-        @foreach ($apartments as $apartment)
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ $apartment->title }}</h5>
-                <p class="card-text">{{ $apartment->description }}.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">N.Stanze: {{ $apartment->rooms }}</li>
-                <li class="list-group-item">N.Letti: {{ $apartment->beds }}</li>
-                <li class="list-group-item">N.Bagni: {{ $apartment->bathrooms }}</li>
-                <li class="list-group-item">{{ $apartment->squareMeters }} mq</li>
-            </ul>
-            {{-- <div class="card-body">
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div> --}}
         </div>
         @endforeach
     </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('Apartment.index') }}">Home</a>
+                </li>
+                {{-- <li class="nav-item">
+                    <a class="nav-link" href="{{ route('Apartment/show/{id}') }}">I tuoi appartamenti</a>
+                </li> --}}
+                <li>
+                    <a class="nav-link" href="{{ route('Apartment.create') }}">Aggiungi un appartamento</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="{{ route('Apartment.edit') }}">Modifica un appartamento</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<!-- Questo è quello che c'era prima -->
+<!-- <div class="container">
         <h2 class="fs-4 text-secondary my-4">
             {{ __('Pannello Utente') }}
         </h2>
@@ -17,10 +40,9 @@
                         <button type="home" class="btn btn-info"><a href="/Apartment/create"
                                 class=" fw-bold text-dark text-decoration-none">Aggiungi appartamento</a></button>
                     </div>
-
-
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
 @endsection
