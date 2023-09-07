1898b9deb4f6dcf5655123a2166b4012643b17fe
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-09-07 14:07:28 +0200

    dashboard button moved,rename 'dashboard'

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <button type="home" class="btn btn-info"><a href="/Apartment/create" class=" fw-bold text-dark text-decoration-none">crea appartamenti</a></button>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Pannello Utente') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card">
 
-                <div class="card-body">
-                   <a href="../../Apartment/myApartments">My Apartments</a>
+
+                    <div class="card-body">
+                        <a href="../../Apartment/myApartments">My Apartments</a>
+                    </div>
+                    <div>
+                        <button type="home" class="btn btn-info"><a href="/Apartment/create"
+                                class=" fw-bold text-dark text-decoration-none">Aggiungi appartamento</a></button>
+                    </div>
+
+
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
