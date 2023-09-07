1ff70de38c78790f9b698cb4de78f7c9d76ff722
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-09-07 17:57:39 +0200

    show,edit changes in controller

--- resources/views/Apartment/index.blade.php
+++ resources/views/Apartment/index.blade.php
@@ -1,78 +1,80 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <!-- Barra di navigazione -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">BoolBNB</a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('Apartment.index') }}">Home</a>
-                </li>
-                {{-- <li class="nav-item">
+        <!-- Barra di navigazione -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">BoolBNB</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('Apartment.index') }}">Home</a>
+                    </li>
+                    {{-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('Apartment/show/{id}') }}">I tuoi appartamenti</a>
                 </li> --}}
-                <li>
-                    <a class="nav-link" href="{{ route('Apartment.create') }}">Aggiungi un appartamento</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="{{ route('Apartment.edit') }}">Modifica un appartamento</a>
-                </li>
-                <li>
-                    <a class="nav-link" href="pagamento">Metti in evidenza</a>
-                </li>
-            </ul>
-            <!-- Ricerca avanzata -->
-            <form class="form-inline ml-auto">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Cerca..." aria-label="Cerca...">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="button">Cerca</button>
+                    <li>
+                        <a class="nav-link" href="{{ route('Apartment.create') }}">Aggiungi un appartamento</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('Apartment.edit') }}">Modifica un appartamento</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="pagamento">Metti in evidenza</a>
+                    </li>
+                </ul>
+                <!-- Ricerca avanzata -->
+                <form class="form-inline ml-auto">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Cerca..." aria-label="Cerca...">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">Cerca</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    </nav>
-
-    <!-- Elenco case vacanze in evidenza -->
-    <h1 class="mt-4">Case Vacanze in Evidenza</h1>
-    <div class="card-deck mt-3">
-        @foreach ($apartments as $apartment)
-        <div class="card">
-            <img src="{{ $apartment->image_url }}" class="card-img-top" alt="{{ $apartment->name }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $apartment->title }}</h5>
-                <p class="card-text">{{ $apartment->description }}</p>
-                <p class="card-text">Location: {{ $apartment->address }}</p>
-                <a href="{{ route('Apartment.show', $apartment->id) }}" class="btn btn-primary">Dettagli</a>
+                </form>
             </div>
-        </div>
-        @endforeach
-    </div>
+        </nav>
 
-    <!-- Questa parte l'ha fatta Matte, chiedi prima di toccarla -->
-    <h1>APARTMENT INDEX</h1>
-    <div class="row row-cols-3">
-        @foreach ($apartments as $apartment)
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ $apartment->title }}</h5>
-                <p class="card-text">{{ $apartment->description }}.</p>
+        <!-- Elenco case vacanze in evidenza -->
+        <section>
+            <h1 class="mt-4">Case Vacanze in Evidenza</h1>
+            <div class="row row-cols-3">
+                @foreach ($apartments as $apartment)
+                    <div class="card-deck mt-3">
+                        <div class="card">
+                            <img src="{{ $apartment->image_url }}" class="card-img-top" alt="{{ $apartment->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $apartment->title }}</h5>
+
+                                <p class="card-text text-secondary">Location: {{ $apartment->address }}</p>
+                                <a href="{{ route('Apartment.show', $apartment->id) }}" class="btn btn-primary">Dettagli</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">N.Stanze: {{ $apartment->rooms }}</li>
-                <li class="list-group-item">N.Letti: {{ $apartment->beds }}</li>
-                <li class="list-group-item">N.Bagni: {{ $apartment->bathrooms }}</li>
-                <li class="list-group-item">{{ $apartment->squareMeters }} mq</li>
-            </ul>
-            {{-- <div class="card-body">
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div> --}}
-        </div>
-        @endforeach
+        </section>
+
+        <!-- Questa parte l'ha fatta Matte, chiedi prima di toccarla -->
+        <section>
+            <h1>Tutti gli Appartamenti</h1>
+            <div class="row row-cols-3">
+                @foreach ($apartments as $apartment)
+                    <div class="card-deck mt-3">
+                        <div class="card">
+                            <img src="{{ $apartment->image_url }}" class="card-img-top" alt="{{ $apartment->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $apartment->title }}</h5>
+
+                                <p class="card-text text-secondary">Location: {{ $apartment->address }}</p>
+                                <a href="{{ route('Apartment.show', $apartment->id) }}"
+                                    class="btn btn-primary">Dettagli</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </section>
+
     </div>
-</div>
 @endsection
--- resources/views/Apartment/show.blade.php
+++ resources/views/Apartment/show.blade.php
@@ -1,11 +1,31 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-            <h1>{{$apartment -> title}}</h1>
-            <div class="card" style="width: 18rem;">
-                <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top" alt="...">
-                
-            </div>
+
+        <h1>{{ $apartment->title }}</h1>
+
+        <img src="{{ asset('storage/' . $apartment->image) }}" alt="...">
+
+        {{-- stanze --}}
+
+        <div>
+            <span>{{ $apartment->rooms }} Stanze</span> &middot;
+            <span> {{ $apartment->beds }} Letti</span> &middot;
+            <span> {{ $apartment->bathrooms }} Bagni</span> &middot;
+            <span> {{ $apartment->squareMeters }} mq</span>
+        </div>
+
+        {{-- descrizione --}}
+        <br>
+        <p>{{ $apartment->description }}.</p>
+
+        {{-- servizi aggiuntivi --}}
+        <h3>Cosa troverai</h3>
+
+        @foreach ($apartment->amenities as $amenity)
+            <span>{{ $amenity->name }}</span>
+        @endforeach
+
 
     </div>
 @endsection
