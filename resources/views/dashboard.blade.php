d002fb98b6e0b63bc453d582363b104af520195f
MatteoGunetto <matteogunetto@tiscali.it>
2023-09-07 09:59:51 +0200

    add button create in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,7 @@
     <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
+    <button type="home" class="btn btn-info"><a href="/Apartment/create" class=" fw-bold text-dark text-decoration-none">crea appartamenti</a></button>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
