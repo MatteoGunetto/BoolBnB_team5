3d9095a9645d17ad3f3f7d8bf5fe6a0f227d0efd
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-09-08 22:50:45 +0200

    routerView installato

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Importazioni pagine vueRouter
+import Home from  "./views.vue/Home.vue";
+import About from  "./views.vue/About.vue";
+
+//Creazione rotte
+const routes = [
+    {  path: '/', component: Home },
+    {  path: '/about', component: About },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+createApp(App).use(router).mount('#app')
---
+++ frontend/src/views.vue/About.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<template>
+    <h1>about</h1>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
