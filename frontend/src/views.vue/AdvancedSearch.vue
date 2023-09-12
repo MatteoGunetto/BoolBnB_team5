8ffa6c2226d977e770e1f55e2152b58843bbd8f8
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-09-12 12:59:41 +0200

    sistemato card in advancedSearch

--- frontend/src/views.vue/AdvancedSearch.vue
+++ frontend/src/views.vue/AdvancedSearch.vue
@@ -21,86 +21,76 @@ export default {
                         </form>
                     </div>
                 </nav>
-
+    
                 <!-- filters -->
                 <!-- camere -->
                 <section class="py-3">
                     <h4>Camere da letto </h4>
                     <nav aria-label="...">
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                                value="option1">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
                             <label class="form-check-label" for="inlineRadio1">1</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">2</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">3</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">4+</label>
                         </div>
-
+    
                     </nav>
                 </section>
-
+    
                 <!-- bagni -->
                 <section class="py-3">
                     <h4>Bagni </h4>
                     <nav aria-label="...">
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                                value="option1">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
                             <label class="form-check-label" for="inlineRadio1">1</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">2</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">3</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                value="option2">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
                             <label class="form-check-label" for="inlineRadio2">4+</label>
                         </div>
-
+    
                     </nav>
                 </section>
-
+    
                 <!-- distanza raggio 20km -->
                 <section class="py-3">
                     <label for="customRange3" class="form-label">
-                        <h4>
-                            Distanza
-                        </h4>
-                    </label>
+                            <h4>
+                                Distanza
+                            </h4>
+                        </label>
                     <input type="range" class="form-range" min="0" max="10" step="0.5" id="customRange3">
                 </section>
-
+    
                 <!-- servizi -->
                 <section>
                     <h4>Servizi </h4>
                     <ul class="list-group">
                         <li class="list-group-item">
-                            <input class="form-check-input me-1" type="radio" name="listGroupRadio" value=""
-                                id="Swimming pool" checked>
+                            <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="Swimming pool" checked>
                             <label class="form-check-label" for="Wi-fi">Wi-fi</label>
                         </li>
                         <li class="list-group-item">
-                            <input class="form-check-input me-1" type="radio" name="listGroupRadio" value=""
-                                id="Swimming pool">
+                            <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="Swimming pool">
                             <label class="form-check-label" for="Swimming pool">Swimming pool</label>
                         </li>
                         <li class="list-group-item">
@@ -109,17 +99,21 @@ export default {
                         </li>
                     </ul>
                 </section>
-
+    
             </div>
             <div class="col-lg-8">
-                qua le cards
+                <div class="row">
+                    <div class="col-md-6 g-3 p-3" v-for="item in 6" :key="item.id">
+                        <Card />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 
-<style lang="scss" >
+<style lang="scss">
 .list-group {
     --bs-list-group-border-color: none;
 }
