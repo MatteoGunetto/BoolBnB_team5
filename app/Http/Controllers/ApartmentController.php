8f50371ccf9cda34581266ae0edf7f529d5e5a86
Antonio <antonioboksic1603@gmail.com>
2023-09-12 10:49:19 +0200

    sistemato controller update, ora aggiorna le coordinate quando si cambia indirizzo

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -34,7 +34,6 @@ class ApartmentController extends Controller
         return view('Apartment.myApartments', ['apartments' => $apartments]);
     }
 
-
     public function show($id)
     {
         $apartment = Apartment::findOrFail($id);
@@ -117,54 +116,59 @@ class ApartmentController extends Controller
          return view('Apartment.edit', compact('apartment', 'amenities'));
     }
 
-
     public function update(Request $request, $id)
     {
+
+        // !!!
+        //CAPIRE SE VOGLIAMO FARE VALIDAZIONI O MENO NELLA UPDATE
+        // !!!
+
         // Cerca il progetto nel database con l'ID specificato
         $apartment = Apartment::findOrFail($id);
+
         // Recupera i dati dal form inviato
         $data = $request->all();
-        // Aggiorna i dati del progetto con i nuovi dati forniti
-        $apartment->update($data);
+
+        //qui prendo la proprietà address di $data e la uso per fare chiamata api con cui ricavo lat e lon
+        // Chiamata tomtom per latitudine e longitudine
+        $address = $data['address'];
+        $apiKey = env('TOMTOM_API_KEY');
+        $endpoint = "https://api.tomtom.com/search/2/geocode/" . urlencode($address) . ".json?key={$apiKey}";
+
+        // la chiamata API la facciamo partire direttamente con la classe
+        $response = Http::get($endpoint);
+
+        //dd($response['results'][0]['position']);
+
+        //vedere se si può scrivere meglio il "path" per position
+        //position è un array associativo dentro alla response che ha 2 "proprietà" : "lat" e "lon" che andiamo a isolare nel passaggio dopo
+        $position = $response['results'][0]['position'];
+
+        //isoliamo "lat" e "lon" come variabili per poterle inserire nel database, cosa che facciamo nel passaggio subito dopo
+        $latitude = $position["lat"];
+        $longitude = $position["lon"];
+
+        $data["latitude"] = $latitude;
+        $data["longitude"] = $longitude;
+
+ 
+        // qua meglio sync che attach, attach si può usare in fase di creazione, sync elimina il collegamento con quelle di cui non trova più dopo la creazione
         $apartment->amenities()->sync($request->input('amenities'));
-  //qua dopo aggiungo commento per spiegare perchè serve sta cosa
-  if($request->hasFile('image')) {
-    // Salva il nuovo file e ottieni il percorso
 
-    $path = $request->file('image')->store('uploads', 'public');
-    //questo path è giusto ma in database me ne mette un altro......
-    //dd($path);
+        //qua dopo aggiungo commento per spiegare perchè serve questa cosa
+        if($request->hasFile('image')) {
+            // Salva il nuovo file e ottieni il percorso
+            $path = $request->file('image')->store('uploads', 'public');
 
-    // Aggiorna il path dell'immagine nel database
-    //QUESTO NON FUNZIONA PERCHè DOPO RISOVRASCRIVO $APARTMENT CON $DATA
-    //QUINDI CIò CHE DEVO FARE QUA DENTRO è MODIFICARE $DATA IN MODO CHE POI AGGIORNO $APARTMENT CON $DATA GIUSTO
-    //questa riga qua sotto è sostanzialmente inutile, non ha senso modificare apartment se poi dopo lo rimodifico con i $data che non sono stati cambiati
-    //se la elimino o se la lascio è la stessa cosa, tanto dopo fuori dall'if $apartment viene sovrascritto quindi quello che faccio qua dentro con apartment non importa.
-    $apartment->image = $path;
+            $data["image"] = $path;
+        }
 
-    //MANCAVA QUESTO BASTARDO, è il $data che devo aggiornare dentro al if, perchè fuori dall'if poi uso $data per aggiornare $apartment
-    $data["image"] = $path;
+        // Salva le modifiche   
+        $apartment->update($data);
 
-}
-// Salva le modifiche
-        $apartment->save();
         // Reindirizza all'URL della vista 'show' per visualizzare il progetto modificato
         return redirect()->route('Apartment.index', $apartment->id);
     }
-
-
-
-
-    // public function destroy(Request $request,$id) {
-
-    //     $apartment = Apartment::findOrFail($id);
-    //     $apartment->amenities()->sync($request->input('amenities'));
-    //     $apartment->messages()->sync($request->input('messages'));
-    //     $apartment->delete();
-
-    //     return redirect()->route('Apartment.myApartments');
-    // }
-
 
     public function destroy(Request $request, $id) {
         $apartment = Apartment::findOrFail($id);
