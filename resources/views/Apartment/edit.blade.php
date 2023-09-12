74b64137130485a8e52298e23173b6340edfa175
MatteoGunetto <matteogunetto@tiscali.it>
2023-09-12 18:10:15 +0200

    add consigli un edit

--- resources/views/Apartment/edit.blade.php
+++ resources/views/Apartment/edit.blade.php
@@ -44,6 +44,11 @@
             <input type="text" id="address" name="address" class="form-control" value="{{ $apartment->address }}">
         </div>
 
+        <!-- suggerimenti address -->
+        <div class="address-option" class="option_none"></div>
+        <div class="address-option"></div>
+        <div class="address-option"></div>
+
         <div class="form-group">
             <!-- <label for="latitude">Latitudine:</label> -->
             <input type="hidden" id="latitude" name="latitude" class="form-control" value="{{ $apartment->latitude }}">
@@ -74,7 +79,7 @@
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" name="amenities[]"
                     id="amenity-{{ $amenity->id }}" value="{{ $amenity->id }}"
-                    {{$apartment->amenities->contains('id', $amenity->id) ? 'checked' : ''}}>                
+                    {{$apartment->amenities->contains('id', $amenity->id) ? 'checked' : ''}}>
                 <label class="form-check-label" for="amenity-{{ $amenity->id }}">
                     {{ $amenity->name }}
                 </label>
@@ -92,6 +97,94 @@
         <button type="submit" class="btn btn-primary">Salva Modifiche</button>
     </form>
 </div>
+
+
+
+<script>
+    // Variabile DOM per l'elemento input.
+    let searchBarDom;
+
+    // Una volta che il DOM è caricato, individua/inizializza la variabile searchBarDom.
+    document.addEventListener("DOMContentLoaded", function() {
+        searchBarDom = document.getElementById("address");
+
+        //questo è un controllo aggiuntivo che non serve nemmno, perchè sappiamo che esiste un id="address" nel nostro codice
+        if (!searchBarDom) {
+            console.error("Elemento 'address' non trovato");
+            return; // Se l'elemento non esiste, esci dalla funzione
+        }
+
+        // Ottieni un riferimento agli elementi con la classe address-option
+        const addressOptionElements = document.querySelectorAll(".address-option");
+
+        // Imposta il testo consigliato come ricerca del indirizzo
+        addressOptionElements.forEach(function(indirizzo){
+            indirizzo.addEventListener("click", function(){
+                searchBarDom.value=indirizzo.textContent
+            });
+        })
+
+
+        let debounceTimer;
+
+        //eventlistener che si attiva quando cambia il valore di searchBarDom che sarebbe il nostro campo input
+        searchBarDom.addEventListener("input", function() {
+            clearTimeout(debounceTimer); // Cancella qualsiasi timer esistente
+            debounceTimer = setTimeout(() => { // Imposta un nuovo timer
+
+                if  (searchBarDom.value.length > 4 ) {
+                    const addressSearched = searchBarDom.value;
+
+                    axios.get('/api/tomtom-proxy', {
+                        params: {
+                            address: addressSearched
+                        }
+                    })
+                    .then(response => {
+                        console.log(searchBarDom.value, "search value")
+                        let suggestedAddresses = response.data.results;
+
+                        addressOptionElements[0].innerHTML = suggestedAddresses[0].address.freeformAddress + ", " + suggestedAddresses[0].address.country;
+                        addressOptionElements[1].innerHTML = suggestedAddresses[1].address.freeformAddress + ", " + suggestedAddresses[1].address.country;
+                        addressOptionElements[2].innerHTML = suggestedAddresses[2].address.freeformAddress + ", " + suggestedAddresses[2].address.country;
+                    })
+                    .catch(error => {
+                        console.error("C'è stato un errore:", error);
+                    });
+
+                    addressOptionElements.forEach(function(element) {
+                        element.style.display = "block";
+                    });
+                }
+
+
+                else {
+                    addressOptionElements.forEach(function(element) {
+                        element.style.display = "none";
+
+                        // questa è una porcheria, per' "quasi" funziona, lascia i bordi dei div
+                        // addressOptionElements[0].innerHTML = ""
+                        // addressOptionElements[1].innerHTML = ""
+                        // addressOptionElements[2].innerHTML = ""
+                    });
+                }
+            }, 500); // Il timer è impostato per mezzo secondo (500 millisecondi)
+
+        });
+
+    });
+</script>
+
+<style>
+    .address-option {
+        display:none;
+        border: var(--bs-border-width) solid var(--bs-border-color);
+    }
+    .option_none{
+        display:none;
+        cursor: pointer
+    }
+</style>
 
 
 @endsection
