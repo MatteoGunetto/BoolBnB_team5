3c3a5b233139463223860e02ece33133ffb05f55
MatteoGunetto <matteogunetto@tiscali.it>
2023-09-15 14:47:17 +0200

    tentativi fix message

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -10,11 +10,23 @@ use App\Models\Message;
 
 class MessageController extends Controller
 {
-    public function messagesIndex()
+    public function messagesIndex (Request $request)
     {
-        $messages = Message::all();
+        // Validazione dei dati inviati dal form (esempio: nome_utente e testo_messaggio)
+        $request->validate([
+            'SenderEmail' => 'required|min:1|max:255',
+            'Name' => 'required|min:1',
+            'Content' => 'required|min:1|max:500',
+        ]);
 
-        return $messages
-    ;
+        // Creazione di un nuovo messaggio utilizzando il modello Message
+        $messages = Message::create([
+            'SenderEmail' => $request->input('SenderEmail'),
+            'Name' => $request->input('Name'),
+            'Content' => $request->input('Content'),
+        ]);
+
+        // Restituisci una risposta con il messaggio creato
+        return response()->json(['message' => 'Messaggio salvato con successo', 'message_data' => $messages], 201);
     }
 }
