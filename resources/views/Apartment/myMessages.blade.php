aa00260edd707973066954906f65967f3c738f40
MatteoGunetto <matteogunetto@tiscali.it>
2023-09-18 18:04:25 +0200

    fix message print

--- resources/views/Apartment/myMessages.blade.php
+++ resources/views/Apartment/myMessages.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<h1>hello world</h1>
 @foreach ($messages as $message)
-<ul>
+
+{{-- <h1>hello world</h1> --}}
+{{-- <ul>
     <pre>{{$message}}</pre>
     <li>
         {{$message->SenderEmail}}
@@ -10,11 +11,42 @@
     <li>
         {{$message->Content}}
     </li>
-</ul>
+</ul> --}}
+<pre>{{$message}}</pre>
+<div class="container py-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="accordion" id="heading{{ $message->id }}">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne{{ $message->id }}" aria-expanded="true" aria-controls="collapseOne{{ $message->id }}">
+                        {{-- {{$apartment->image}} --}}
+                        {{$message->SenderEmail}}
+                        <br>
+                    {{$message->created_at}}
+                    </button>
+                  </h2>
+                  <div id="collapseOne{{ $message->id }}" class="accordion-collapse collapse show" aria-labelledby="heading{{ $message->id }}" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      <strong>{{$message->Name}}</strong>
+                      <p>
+                        {{$message->Content}}
+                    </p>
+                    <br>
+                    <p>{{$message->created_at}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+</div>
+@endforeach
 
 
 
-            @endforeach
+
 
 
 
