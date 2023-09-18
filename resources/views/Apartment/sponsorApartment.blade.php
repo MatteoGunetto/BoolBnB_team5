bd7dae8d9ea60e2b615ec9d3e03ac92f4dc64e4f
Antonio <antonioboksic1603@gmail.com>
2023-09-18 09:45:27 +0200

    iniziato a inserire roba di braintree

--- resources/views/Apartment/sponsorApartment.blade.php
+++ resources/views/Apartment/sponsorApartment.blade.php
@@ -9,12 +9,10 @@
         <div>
             con la durata di :
             <strong>{{$promotion->durationInDays}}  giorni</strong>
-
         </div>
         <div>
             al costo di :
             <strong>{{$promotion->cost}}  Euro</strong>
-
         </div>
         <div>
             per l'appartamento :
@@ -23,5 +21,33 @@
     </div>
 </div>
 
+<!-- Container di Braintree -->
+<div id="dropin-container"></div>
+<button id="submit-button">Effettua il pagamento</button>
+
+<script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
+
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        braintree.dropin.create({
+            authorization: 'YOUR_CLIENT_TOKEN_FROM_SERVER', // Sostituire con il token effettivo ottenuto dal server
+            container: document.getElementById('dropin-container'),
+            // ...plus remaining configuration (qui puoi inserire ulteriori configurazioni se necessario)
+        }).then((dropinInstance) => {
+            const button = document.querySelector('#submit-button');
+            button.addEventListener('click', function () {
+                dropinInstance.requestPaymentMethod(function (err, payload) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    // Qui puoi inviare `payload.nonce` al tuo server per elaborare il pagamento
+                });
+            });
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+</script>
 
 @endsection
