8e7bcd8854f585d0c4b37b80f655cf303e256c3b
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-09-19 15:44:12 +0200

    tutto funzionante

--- resources/views/Apartment/myApartments.blade.php
+++ resources/views/Apartment/myApartments.blade.php
@@ -13,9 +13,9 @@
                 <div class="col">
                     <div class="card">
 
-                        <i class="actionReveal bi bi-three-dots position-absolute top-0 end-0 text-white me-2"  data-target="actionPanel-{{ $apartment->id }}" style="font-size: 1.8rem;"></i>
+                        <i class="actionReveal bi bi-three-dots text-white position-absolute top-0 end-0 me-2"  data-target="actionPanel-{{ $apartment->id }}" style="font-size: 1.8rem; z-index:20;"></i>
                     <!-- Bottoni edit ed elimina -->
-                    <ul id="actionPanel-{{ $apartment->id }}" class="list-group list-group-flush position-absolute top-0 end-0 d-none">
+                    <ul id="actionPanel-{{ $apartment->id }}" class="actionPanel list-group list-group-flush position-absolute top-0 end-0 d-none" style="z-index:10">
                         <li class="list-group-item">
 
                             <!-- BOTTONE EDIT -->
@@ -25,41 +25,43 @@
                             </a>
                         </li>
                         <li class="list-group-item">
-                        <form class="d-inline" method="POST" action="{{ route('Apartment.destroy', $apartment->id) }}">
-                                    @csrf
-                                    @method('DELETE')
+                            <form id="deleteForm" class="d-inline" method="POST" action="{{ route('Apartment.destroy', $apartment->id) }}">
+                                        @csrf
+                                        @method('DELETE')
 
-                                    <!-- bottone collegato alla modale -->
-                                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#delete_modal_{{ $apartment->id }}">
-                                        <i class="bi bi-trash3"></i>
-                                        Delete
-                                    </button>
-                                </form>
+                                <!-- bottone collegato alla modale -->
+                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#delete_modal_{{ $apartment->id }}">
+                                    <i class="bi bi-trash3"></i>
+                                    Delete
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="delete_modal_{{ $apartment->id }}" tabindex="-1" aria-labelledby="deleteModal_{{ $apartment->id }}" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="deleteModal_{{ $apartment->id }}">Sei sicuro di voler eliminare l'appartamento:</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <strong>
+                                                {{$apartment->title}}
+                                                </strong>
+                                            ?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <!-- bottoni della modale -->
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                                <button id="submitButton" type="submit" class="btn btn-danger">Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </li>
                     </ul>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="delete_modal_{{ $apartment->id }}" tabindex="-1" aria-labelledby="deleteModal_{{ $apartment->id }}" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="deleteModal_{{ $apartment->id }}">Sei sicuro di voler eliminare l'appartamento:</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <strong>
-                                    {{$apartment->title}}
-                                    </strong>
-                                ?
-                                </div>
-                                <div class="modal-footer">
-                                    <!-- bottoni della modale -->
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                    <button type="submit" class="btn btn-danger">Elimina</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
                        
                     <!-- IMMAGINE -->
                         {{-- se l'img è vuota allora mettine una di default --}}
@@ -88,33 +90,31 @@
                     </div>
                 </div>
             @endforeach
-
         </div>
     </div>
-
-
 <script>
 
-//  // Ottieni tutti gli elementi con la classe "mostra-tabella"
-//  var buttons = document.querySelectorAll(".actionReveal");
+ // Ottieni tutti gli elementi con la classe "mostra-tabella"
+ var buttons = document.querySelectorAll(".actionReveal");
 
-// // Aggiungi un gestore di eventi a ciascun pulsante
-// buttons.forEach(function (button) {
-//     button.addEventListener("click", function () {
-//         // Ottieni l'ID della tabella target dall'attributo "data-target"
-//         var targetId = button.getAttribute("data-target");
+// Aggiungi un gestore di eventi a ciascun pulsante
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        // Ottieni l'ID della tabella target dall'attributo "data-target"
+        var targetId = button.getAttribute("data-target");
 
-//         // Ottieni l'elemento tabella target
-//         var table = document.getElementById(targetId);
-//         table.classList.toggle('d-none');
-//         table.classList.add('is-active');
+        // Ottieni l'elemento tabella target
+        var table = document.getElementById(targetId);
+        table.classList.toggle('d-none');
+        button.classList.toggle('text-white')
 
-//     });
-// });
+    });
+});
+
 
     
     // // Aggiungi un gestore di eventi al pulsante "Conferma"
-    // document.getElementById("confirmDelete").addEventListener("click", function () {
+    // document.getElementById("confirmDelete").addEventListener("click", () => {
     //    // Qui inserisci il codice per l'eliminazione effettiva dell'elemento
     //    // Puoi utilizzare AJAX, una richiesta HTTP, o qualsiasi altro metodo necessario per l'eliminazione.
 
@@ -123,33 +123,9 @@
     // });
 
 
-    document.body.addEventListener("click", function (event) {
-    var buttons = document.querySelectorAll(".actionReveal");
-    // Verifica se il clic è avvenuto su un pulsante con la classe "actionReveal"
-    if (event.target.classList.contains("actionReveal")) {
-        var targetId = event.target.getAttribute("data-target");
-        var table = document.getElementById(targetId);
-        // Chiudi tutti i pannelli delle azioni aperti
-        var closeList = document.querySelectorAll(".is-active");
-        closeList.forEach(function (close) {
-            close.classList.add("d-none");
-            close.classList.remove("is-active");
-        });
-        // Apri il pannello delle azioni corrispondente al pulsante cliccato
-        if (table) {
-            table.classList.toggle("d-none");
-            table.classList.add("is-active");
-        }
-    } else {
-        // Chiudi tutti i pannelli delle azioni aperti se il clic è al di fuori dei pulsanti "actionReveal"
-        var closeList = document.querySelectorAll(".is-active");
-        closeList.forEach(function (close) {
-            close.classList.add("d-none");
-            close.classList.remove("is-active");
-        });
-    }
-});
-    
+
+
+
 
 </script>
 
