e354e876f206e910e74f5c14184db254b1bf0d3c
FRAALES <alessandrofranco1992@gmail.com>
2023-09-20 17:54:44 +0200

    altezza card deve stare fissa e non ci sta

--- frontend/src/components/elements/Card.vue
+++ frontend/src/components/elements/Card.vue
@@ -16,37 +16,37 @@ export default {
 </script>
 
 <template>
-    <div class="card">
-        <img :src="`${store.urlImg}${cardProp.image}`" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="text-primary">{{ cardProp.title }}</h5>
-            <p class="card-text mb-4">{{ cardProp.description }}</p>
-            <div class="icon-container d-flex position-relative mt-3">
-                <div class="icon d-flex align-items-center">
-                    <img src="../../../public/icon-bed.svg" alt="icona-letto">
-                    <span class="ms-2">{{ cardProp.beds }}</span>
-                </div>
-                <div class="icon d-flex align-items-center ms-2">
-                    <img src="../../../public/icon-bathroom.svg" alt="icona-bagno">
-                    <span class="ms-2">{{ cardProp.bathrooms }}</span>
-                </div>
-                <div class="icon d-flex align-items-center ms-2">
-                    <img src="../../../public/icon-rooms.svg" alt="icona-appartamento">
-                    <span class="ms-2">{{ cardProp.rooms }}</span>
-                </div>
-                
-              
-                <div v-if="cardProp.distance" class="icon d-flex align-items-center ms-2 align-self-end">
-                    <span class="ms-2">
-                        <span>
-                            Distance:
+    <div class="card-group">
+
+        <div class="card">
+            <img :src="`${store.urlImg}${cardProp.image}`" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-primary">{{ cardProp.title }}</h5>
+                <p class="card-text mb-4">{{ cardProp.description }}</p>
+                <div class="icon-container d-flex position-relative mt-3 card-footer">
+                    <div class="icon d-flex align-items-center">
+                        <img src="../../../public/icon-bed.svg" alt="icona-letto">
+                        <span class="ms-2">{{ cardProp.beds }}</span>
+                    </div>
+                    <div class="icon d-flex align-items-center ms-2">
+                        <img src="../../../public/icon-bathroom.svg" alt="icona-bagno">
+                        <span class="ms-2">{{ cardProp.bathrooms }}</span>
+                    </div>
+                    <div class="icon d-flex align-items-center ms-2">
+                        <img src="../../../public/icon-rooms.svg" alt="icona-appartamento">
+                        <span class="ms-2">{{ cardProp.rooms }}</span>
+                    </div>
+
+
+                    <div v-if="cardProp.distance" class="icon d-flex align-items-center ms-2 align-self-end">
+                        <span class="ms-2">
+                            <span>
+                                Distance:
+                            </span>
+                            {{ cardProp.distance.toFixed(2) }} km
                         </span>
-                        {{ cardProp.distance.toFixed(2) }} km
-                    </span>
+                    </div>
                 </div>
-                
-
-
             </div>
         </div>
     </div>
