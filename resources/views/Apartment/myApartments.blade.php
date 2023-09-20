847db14d94a418db0ede6e4122d4d0ff68b20516
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-09-20 12:54:31 +0200

    update sidebar in my apartments

--- resources/views/Apartment/myApartments.blade.php
+++ resources/views/Apartment/myApartments.blade.php
@@ -1,38 +1,46 @@
 @extends('layouts.app')
 @section('content')
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="dash-nav bg-primary p-md-4 py-4 vh-100" id="sidebar" style="--bs-bg-opacity: .8;">
-                    <ul class="nav flex-column  gap-2">
-                        <li class="nav-item">
-                            <a class="btn btn-primary" href="{{ route('Apartment.myApartments') }}">
-                                <i class="bi bi-house me-md-2"></i><span>Appartamenti</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="btn btn-primary" href="{{ route('Apartment.myMessages') }}">
-                                <i class="bi bi-envelope-paper me-md-2"></i> <span>Messagi</span>
-                            </a>
-                        </li>
-                    </ul>
+    <div class="row ">
+        {{-- side bar --}}
+        <div class="col-lg-2 col-sm-2">
+            <div class="dash-nav bg-primary p-md-4 py-4 vh-100" id="sidebar" style="--bs-bg-opacity: .8;">
+                <ul class="nav flex-column  gap-2">
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="{{ route('Apartment.myApartments') }}">
+                            <i class="bi bi-house me-md-2"></i><span>Appartamenti</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="{{ route('Apartment.myMessages') }}">
+                            <i class="bi bi-envelope-paper me-md-2"></i> <span>Messagi</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        {{-- contenuto --}}
+        <div class="col-lg-10 col-sm-10 g-4">
+            {{-- titolo i tuoi appartamenti --}}
+            <div class="mb-4">
+                <div class="d-flex justify-content-between align-items-center flex-wrap">
+                    <h1 class="">I tuoi appartamenti</h1>
+                    <a class="btn btn-outline-primary" href="{{ route('Apartment.create') }}">Aggiungi un appartamento <i
+                            class="bi bi-plus-lg"></i>
+                    </a>
                 </div>
             </div>
-            <div class="col text-start">
-                <div class="mb-4">
-                    <div class="d-flex justify-content-between align-items-center flex-wrap">
-                        <h1 class="">I tuoi appartamenti</h1>
-                        <a class="btn btn-outline-primary" href="{{ route('Apartment.create') }}">Aggiungi un appartamento  <i class="bi bi-plus-lg"></i></a>
-                    </div>
-                </div>
-                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 
-                    @foreach ($apartments as $apartment)
-                        <div class="col">
-                            <div class="card">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                {{-- cards dei miei appartamenti --}}
+                @foreach ($apartments as $apartment)
+                    <div class="col col-sm-6">
+                        <div class="card">
 
-                                <i class="actionReveal bi bi-three-dots text-white position-absolute top-0 end-0 me-2"  data-target="actionPanel-{{ $apartment->id }}" style="font-size: 1.8rem; z-index:20;"></i>
+                            <i class="actionReveal bi bi-three-dots text-white position-absolute top-0 end-0 me-2"
+                                data-target="actionPanel-{{ $apartment->id }}" style="font-size: 1.8rem; z-index:20;"></i>
                             <!-- Bottoni edit ed elimina -->
-                            <ul id="actionPanel-{{ $apartment->id }}" class="actionPanel list-group list-group-flush position-absolute top-0 end-0 d-none">
+                            <ul id="actionPanel-{{ $apartment->id }}"
+                                class="actionPanel list-group list-group-flush position-absolute top-0 end-0 d-none">
                                 <li class="list-group-item">
 
                                     <!-- BOTTONE EDIT -->
@@ -42,34 +50,41 @@
                                     </a>
                                 </li>
                                 <li class="list-group-item">
-                                    <form id="deleteForm" class="d-inline" method="POST" action="{{ route('Apartment.destroy', $apartment->id) }}">
-                                                @csrf
-                                                @method('DELETE')
+                                    <form id="deleteForm" class="d-inline" method="POST"
+                                        action="{{ route('Apartment.destroy', $apartment->id) }}">
+                                        @csrf
+                                        @method('DELETE')
 
                                         <!-- bottone collegato alla modale -->
-                                        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#delete_modal_{{ $apartment->id }}">
+                                        <button type="button" class="btn" data-bs-toggle="modal"
+                                            data-bs-target="#delete_modal_{{ $apartment->id }}">
                                             <i class="bi bi-trash3"></i>
                                             Delete
                                         </button>
 
                                         <!-- Modal -->
-                                        <div class="modal fade" id="delete_modal_{{ $apartment->id }}" tabindex="-1" aria-labelledby="deleteModal_{{ $apartment->id }}" aria-hidden="true">
+                                        <div class="modal fade" id="delete_modal_{{ $apartment->id }}" tabindex="-1"
+                                            aria-labelledby="deleteModal_{{ $apartment->id }}" aria-hidden="true">
                                             <div class="modal-dialog">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h1 class="modal-title fs-5" id="deleteModal_{{ $apartment->id }}">Sei sicuro di voler eliminare l'appartamento:</h1>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        <h1 class="modal-title fs-5" id="deleteModal_{{ $apartment->id }}">
+                                                            Sei sicuro di voler eliminare l'appartamento:</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
                                                         <strong>
-                                                        {{$apartment->title}}
+                                                            {{ $apartment->title }}
                                                         </strong>
-                                                    ?
+                                                        ?
                                                     </div>
                                                     <div class="modal-footer">
                                                         <!-- bottoni della modale -->
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                                        <button id="submitButton" type="submit" class="btn btn-danger">Elimina</button>
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Annulla</button>
+                                                        <button id="submitButton" type="submit"
+                                                            class="btn btn-danger">Elimina</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -81,35 +96,40 @@
 
 
                             <!-- IMMAGINE -->
-                                {{-- se l'img è vuota allora mettine una di default --}}
-                                @if (!empty($apartment->image))
-                                    <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top" alt="Apartment Image">
-                                @else
-                                    <img src="{{ asset('storage/default_image.png') }}" class="card-img-top" style="height: 280px "
-                                        alt="Default Image">
-                                @endif
+                            {{-- se l'img è vuota allora mettine una di default --}}
+                            @if (!empty($apartment->image))
+                                <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top"
+                                    alt="Apartment Image">
+                            @else
+                                <img src="{{ asset('storage/default_image.png') }}" class="card-img-top"
+                                    style="height: 280px " alt="Default Image">
+                            @endif
 
-                                <!-- BODY DELLA CARD -->
+                            <!-- BODY DELLA CARD -->
 
-                                <div class="card-body">
+                            <div class="card-body">
 
-                                    <h5 class="card-title">{{ $apartment->title }}</h5>
+                                <h5 class="card-title">{{ $apartment->title }}</h5>
 
-                                    <p class="card-text">Location: {{ $apartment->address }}</p>
+                                <p class="card-text">Location: {{ $apartment->address }}</p>
 
-                                    <!-- BOTTONE DETTAGLI -->
+                                <!-- BOTTONE DETTAGLI -->
 
-                                    <!-- Modal dettagli -->
-                                    <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#detailModal-{{ $apartment->id }}">
+                                <!-- Modal dettagli -->
+                                <button type="button" class="btn btn-outline-info" data-bs-toggle="modal"
+                                    data-bs-target="#detailModal-{{ $apartment->id }}">
                                     Dettagli
-                                    </button>
+                                </button>
 
-                                    <!-- Modal -->
-                                    <div class="detailModal modal fade" id="detailModal-{{ $apartment->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                            <div class="modal-header flex-column justify-content-end align-items-start" style="background-image: linear-gradient(216deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.5998993347338936) 100%), url('{{ asset('storage/' . $apartment->image) }}');">
-                                                <h1 class="modal-title fs-3 text-white" id="exampleModalLabel"> {{$apartment->title}} </h1>
+                                <!-- Modal -->
+                                <div class="detailModal modal fade" id="detailModal-{{ $apartment->id }}" tabindex="-1"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header flex-column justify-content-end align-items-start"
+                                                style="background-image: linear-gradient(216deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.5998993347338936) 100%), url('{{ asset('storage/' . $apartment->image) }}');">
+                                                <h1 class="modal-title fs-3 text-white" id="exampleModalLabel">
+                                                    {{ $apartment->title }} </h1>
                                                 <div class="modal-icons d-flex gap-2 text-white">
                                                     <div class="service-container">
                                                         Letti: {{ $apartment->beds }}
@@ -121,73 +141,67 @@
                                                         Stanze: {{ $apartment->rooms }}
                                                     </div>
                                                 </div>
-                                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                <button type="button" class="btn-close btn-close-white"
+                                                    data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
                                                 <!-- Da rivedere -->
-                                                {{$apartment->amenities}}
+                                                {{ $apartment->amenities }}
                                                 <h5>Descrizione</h5>
-                                                {{$apartment->description}}
-                                            </div>
+                                                {{ $apartment->description }}
                                             </div>
                                         </div>
                                     </div>
-                                    <!-- <a href="{{ route('Apartment.show', $apartment->id) }}" class="btn btn-outline-info">Dettagli</a> -->
-
-                                    <!-- BOTTONE SPONSOR -->
-                                    <a href="{{ route('Apartment.selectSponsorship', $apartment->id) }}" class="btn btn-warning">Sponsor</a>
-
-
                                 </div>
+                                <!-- <a href="{{ route('Apartment.show', $apartment->id) }}" class="btn btn-outline-info">Dettagli</a> -->
+
+                                <!-- BOTTONE SPONSOR -->
+                                <a href="{{ route('Apartment.selectSponsorship', $apartment->id) }}"
+                                    class="btn btn-warning">Sponsor</a>
+
+
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
         </div>
+    </div>
 
-<script>
+    <script>
+        // Ottieni tutti gli elementi con la classe "mostra-tabella"
+        var buttons = document.querySelectorAll(".actionReveal");
 
- // Ottieni tutti gli elementi con la classe "mostra-tabella"
- var buttons = document.querySelectorAll(".actionReveal");
+        // Aggiungi un gestore di eventi a ciascun pulsante
+        buttons.forEach(function(button) {
+            button.addEventListener("click", function() {
+                // Ottieni l'ID della tabella target dall'attributo "data-target"
+                var targetId = button.getAttribute("data-target");
 
-// Aggiungi un gestore di eventi a ciascun pulsante
-buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        // Ottieni l'ID della tabella target dall'attributo "data-target"
-        var targetId = button.getAttribute("data-target");
+                // Ottieni l'elemento tabella target
+                var table = document.getElementById(targetId);
+                table.classList.toggle('d-none');
+                button.classList.toggle('text-white')
 
-        // Ottieni l'elemento tabella target
-        var table = document.getElementById(targetId);
-        table.classList.toggle('d-none');
-        button.classList.toggle('text-white')
-
-    });
-});
-
-
-
-    // // Aggiungi un gestore di eventi al pulsante "Conferma"
-    // document.getElementById("confirmDelete").addEventListener("click", () => {
-    //    // Qui inserisci il codice per l'eliminazione effettiva dell'elemento
-    //    // Puoi utilizzare AJAX, una richiesta HTTP, o qualsiasi altro metodo necessario per l'eliminazione.
-
-    //    // Chiudi la modale dopo l'eliminazione
-    //    $("#myModal").modal("hide");
-    // });
+            });
+        });
 
 
 
+        // // Aggiungi un gestore di eventi al pulsante "Conferma"
+        // document.getElementById("confirmDelete").addEventListener("click", () => {
+        //    // Qui inserisci il codice per l'eliminazione effettiva dell'elemento
+        //    // Puoi utilizzare AJAX, una richiesta HTTP, o qualsiasi altro metodo necessario per l'eliminazione.
 
-
-
-</script>
+        //    // Chiudi la modale dopo l'eliminazione
+        //    $("#myModal").modal("hide");
+        // });
+    </script>
 
 
     <style>
         .sponsored {
-    border: 2px solid gold;
-}
+            border: 2px solid gold;
+        }
     </style>
 @endsection
-
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
 
     <!-- CDN AXIOS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.0/axios.min.js" integrity="sha512-aoTNnqZcT8B4AmeCFmiSnDlc4Nj/KPaZyB5G7JnOnUEkdNpCZs1LCankiYi01sLTyWy+m2P+W4XM+BuQ3Q4/Dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.0/axios.min.js"
+        integrity="sha512-aoTNnqZcT8B4AmeCFmiSnDlc4Nj/KPaZyB5G7JnOnUEkdNpCZs1LCankiYi01sLTyWy+m2P+W4XM+BuQ3Q4/Dg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,8 +19,12 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -42,7 +48,9 @@
                 {{-- config('app.name', 'Laravel') --}}
                 </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -57,33 +65,36 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('Apartment/myApartments') }}">{{ __('My Apartment') }}</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item"
+                                        href="{{ url('Apartment/myApartments') }}">{{ __('My Apartment') }}</a>
+                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
@@ -95,9 +106,9 @@
 
                 </div>
             </div> -->
-<div class="dasboard d-flex">
+        <div class="dasboard d-flex">
 
-<!-- Barra laterale -->
+            <!-- Barra laterale -->
 
             {{-- <div class="dash-nav bg-primary p-md-4 py-4 vh-100" id="sidebar" style="--bs-bg-opacity: .8;">
                 <ul class="nav flex-column  gap-2">
@@ -114,9 +125,9 @@
                 </ul>
             </div> --}}
 
-            <main class="dash-main vh-100">
-        @yield('content')
-        </main>
+            <main class=" vh-100">
+                @yield('content')
+            </main>
 
 </body>
 
